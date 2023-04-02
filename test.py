ed74078e720ee74572cace4610b23f1e66e881fa
hyuan12 <hyuan12@stevens.edu>
2023-04-02 14:44:35 -0400

    2.2

--- test.py
+++ test.py
@@ -136,7 +136,7 @@ def get_action():
         # except KeyboardInterrupt:
         #     do_quit("")
         except EOFError:
-            print("Use 'quit' to exit.")
+            print("\nUse 'quit' to exit.")
             continue
 
         if not u_input:
@@ -150,6 +150,8 @@ def get_action():
             # directions become verbs
             elif verb in gs.directions:
                 do_go(gs.get_direction(verb))
+            elif verb == "^d":
+                print("\nUse 'quit' to exit.")
             else:
                 print("That command was not recognized")
 
